fb682ce68480591fa7bc305a75cf9908938cf509
Admin <your_github_email@example.com>
2025-12-02 20:26:42 +0530

    initial work

---
+++ regular_exp.py
@@ -0,0 +1,28 @@
+'''import re
+text = "Python is very easy"
+regex = r"Python"
+match = re.match(regex,text)
+# print(match)
+print(match.span())
+start,end = match.span()
+print(text[start:end])
+print(text[match.start():match.end()])'''
+
+# import re
+# text = "Iranna is a good boy"
+# regex = r"Iranna"
+# match = re.match(regex,text)
+# print(match)
+# print(match.span())
+# start,end = match.span()
+# print(text[match.start():match.end()]) #Match also cantains a position, regeular exp, string
+# print(text[start:end])
+# print(text[match.start():match.end()])
+
+import re
+text = "Python is very easy"
+regu = r"very"
+match = re.match(regu,text)
+print(match)
+print(text[match.start():match.end()])
+
--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-def sum():
-    '''this sum function is for addition'''
-    a = 10
-    b = 20
-    c = a+b
-    print(c)
+# def sum():
+#     '''this sum function is for addition'''
+#     a = 10
+#     b = 20
+#     c = a+b
+#     print(c)
 
-def sub():
-    '''this function is used for subtraction'''
-    a = 10
-    b = 20
-    c = a-b
-    print(c)
+# def sub():
+#     '''this function is used for subtraction'''
+#     a = 10
+#     b = 20
+#     c = a-b
+#     print(c)
 
 
-def mul():
-    '''this function is used for multiply'''
-    a = 10
-    b = 20
-    c = a**b
+# def mul():
+#     '''this function is used for multiply'''
+#     a = 10
+#     b = 20
+#     c = a**b
 
-    print(c)
+#     print(c)
 
