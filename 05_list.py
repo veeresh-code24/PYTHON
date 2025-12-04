cc4e74ede34e40f37dfe22d49b8ba83acba1f5da
veeresh-code24 <veereshkanamadi@gmail.com>
2025-12-04 18:32:11 +0530

    today work

--- 05_list.py
+++ 05_list.py
@@ -41,8 +41,8 @@ print(lst[7:1:-1])'''
 # for i in range(0,8):
 #     print(lst[i])
 
-lst = [10,20,30,40,50]
-print(lst)
+# lst = [10,20,30,40,50]
+# print(lst)
 
 # lst.append(40)
 # print(lst)
@@ -53,7 +53,7 @@ print(lst)
 # lst = lst + [10,20,30]
 # print(lst)
 
-lst.extend([10,20,30])
+'''lst.extend([10,20,30])
 print(lst)
 
 lst.insert(2,99)
@@ -66,8 +66,45 @@ lst[2] = 300
 print(lst)
 
 lst = lst + [1,2,3]
+print(lst)'''
+
+# lst = [10,20,30,40,50]
+# lst[1:4] = [90]
+# print(lst)
+
+# lst[1:4] = [99,99,99,99,99,99,99]
+# print(lst)
+
+# lst[::2] = [22,22,22]
+# print(lst)
+
+# lst  = [10,20,30,40,50,30,40]
+# print(lst)
+# lst.remove(30)
+# print(lst)
+
+# while 30 in lst:
+#     lst.remove(30)
+# print(lst)
+
+# print(30 in lst)
+# print(30 not in lst)
+
+# print(lst.pop())
+# print(lst)
+# print(lst)
+
+
+lst  = [10,20,30,40,50,30,40]
+# lst.pop(0)
+# print(lst)
+# del lst[-1:-7:-1]
+
+# del lst[::3]
+
+# print(lst)
+
+lst[2:6] = [4]
 print(lst)
-
-
 
 
