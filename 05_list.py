b7a780555a95f8d3447c029efc9fa3bee3d956d1
veeresh-code24 <veereshkanamadi@gmail.com>
2025-12-22 17:45:35 +0530

    initial work

--- 05_list.py
+++ 05_list.py
@@ -346,17 +346,59 @@ lst = [2,4,6,8,10,2,4,81,3,7,9]
 # sq_lst = [i**2 for i in lst if i%2 == 0]
 # print(sq_lst)
 
-lst1 = ["name","age","Branch"]
-lst2 = ["iranna",21,"Enginee"]
+# lst1 = ["name","age","Branch"]
+# lst2 = ["iranna",21,"Enginee"]
 # print(list(zip(lst1,lst2)))
 # print(list(zip(lst1,lst2)))
 
-for i,j in enumerate(lst1,start=1):
-    print(i,j) 
+# for i,j in enumerate(lst1,start=1):
+#     print(i,j) 
+
+
+# lst1 = ["A","app","","da","kee","t","doc","a"]
+# lst2 = ["n","le","a","y","ps","he","tor","way"]
+
+# res = []
+# for i,j in zip(lst1,lst2):
+#     res.append(i+j)
+# print(" ".join(res))
+
+# print(" ".join([i+j for i,j in zip(lst1,lst2)]))
+
+# print(" ".join([i+j for i,j in zip(lst1,lst2)]))
+
+# s = input("Enter your string\n")
+
+# lst = s.split()
+
+# res = []
+# for i in range(len(lst)):
+#     if len(lst[i]) > 5:
+#         res.append(lst[i].lower())
+#     else:
+#         res.append(lst[i].upper())
+# print(' '.join(res))
+
+# print(''.join([lst[i].lower() if len(lst[i]) > 5 else lst[i].upper() for i in range(len(lst))]))
+
+s = input("Enter your string\n")
+lst = s.split()
+
+# res = []
+# for i in range(len(lst)):
+#     if len(lst[i]) > 5:
+#         res.append(lst[i].lower())
+#     else:
+#         res.append(lst[i].upper())
+# print(' '.join(res))
+
+print(' '.join([lst[i].lower() if len(lst[i]) > 5 else lst[i].upper() for i in range(len(lst))]))
 
 
 
-    
+
+
+     
 
     
 
