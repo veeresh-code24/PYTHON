e4e50c0122c3908b3696876ec87d64f50861229f
veeresh-code24 <veereshkanamadi@gmail.com>
2025-12-31 11:33:22 +0530

    Today work

--- 07_dictionary.py
+++ 07_dictionary.py
@@ -1,95 +1,27 @@
-# a = {
-#     "iranna": 98,
-#     "manya": 67,
-#     "pacchu": 76
-# }
-# print(a["iranna"])
-# # # print(a.get("iranna"))
+# d = {2:36,1:32,3:45,4:57,5:73}
+# print(d)
+# print(d[2])
+# print(d[3])
+# print(d["two"])
 
-# print(a.keys())
-# print(a.values())
-# a.update({"iranna" : 78, "veeresh": 87})
-# print(a)
-# print(a.items())
-# print(a.get("iranna"))
-# print(a["iranna"])
+d = {1:"c",2:"java",3:'python'}
+print(d)
 
-# a["kalasha"] = 98
-# print(a)
+# Adiing one element
+d[4] = "c++"
+print(d)
+
+# Adding two element in two ways
+d.update({'python':'PHP',6:'javascript'})
+print(d)
 
 
-person = dict(name="iranna", age=20, manya=98)
-# print(person.popitem())
-# print(person.pop(0))
-# print(person)
-# person.clear()
-# print(person)
+d.update(seven='JS',eight='Bootcamp')
+print(d)
 
-# del person["name"]
-person  ["name"] = "pacchu"
-
-# del person ["age"]
-print(person)
-person["branch"] = "priya"
-print(person)
-
-# person = dict(name="iranna", age=20, manya=98)
+# Modifying a element
+d[2] = "Python"
+print(d)
 
 
-# for key, value in person.items(): 
-#     print(f"{key}:{value}")   
 
-
-# for key in person:
-#     print(key)
-# for value in person.values():
-#     print(person) 
-
-# a = {
-#     "s1" : {"name": "iranna", "age": 20},
-#     "s2" : {"name": "chandu", "age": 21}
-# }
-
-# print(a["s2"])
-# print(a["s1"])
-# a.get("s1")
-# print(a["s1"])
-
-
-# marks = {
-#     "harry": 98,
-#     "iranna": 65,
-#     "pacchu": 76
-# }
-# print(marks, type(marks))
-# print(marks.keys())
-# print(marks.values())
-# print(marks.items())
-# print(marks["harry"])
-# print(marks.get("iranna"))
-# marks.update({"iranna": 98, "manya": 87})
-# marks["iranna"] = 84
-
-# print(marks)
-# marks["age"] = 21
-# print(marks)
-
-# a = {
-#      "101": {"name": "iranna", "Age": 21, "branch": "Engineering"},
-#      "102": {"name": "chandu", "Age": 22, "Branch": "BCA"}
-# }
-
-# print(a["101"]["name"])
-# a["103"] = {"name": "akash", "Age": 22, "Branch": "bba"}
-# print(a)
-
-# a.pop("102")
-# print(a)
-
-# a = {
-#     "102": {"name": "iranna", "age": 21, "branch": "Engineering"}
-# }
-# # print(a)
-# print(a.popitem())
-# del a ["name"]
-# print(a)
