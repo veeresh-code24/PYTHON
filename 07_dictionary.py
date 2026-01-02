6ce6638d04b4d2d94a07cac8a4793be68fe3bc90
veeresh-code24 <veereshkanamadi@gmail.com>
2026-01-02 20:17:16 +0530

    initial work

--- 07_dictionary.py
+++ 07_dictionary.py
@@ -4,7 +4,7 @@
 # print(d[3])
 # print(d["two"])
 
-d = {1:"c",2:"java",3:'python'}
+'''d = {1:"c",2:"java",3:'python'}
 print(d)
 
 # Adiing one element
@@ -21,7 +21,35 @@ print(d)
 
 # Modifying a element
 d[2] = "Python"
-print(d)
+print(d)'''
+
+# d = {1:22, 2:33, 3:44, 4:55, 5:66}
+# print(d)
+
+# d.pop(99,999)
+# print(d)
+
+# d.popitem()
+# print(d)
+
+# print(d.pop(1))
+
+# del d[6]
+# print(d)
+
+# print(d.clear())
+
+# print(d.pop(2, "python"))
+# d.clear()
+# print(d)
+
+# d.popitem()
+# print(d)
+
+
+
+
+
 
 
 
