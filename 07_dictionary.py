6486af2ef18e574cb2fb1c23ca1836630bfb06bc
veeresh-code24 <veereshkanamadi@gmail.com>
2026-01-03 22:46:12 +0530

    today work

--- 07_dictionary.py
+++ 07_dictionary.py
@@ -46,8 +46,8 @@ print(d)'''
 # d.popitem()
 # print(d)
 
-d = {1:'a',2:[10,20,30] }
-print(d[1])
+# d = {1:'a',2:[10,20,30] }
+# print(d[1])
 
 # x = "a"
 # print(x)
@@ -102,15 +102,95 @@ print(d[1])
 #     if d[i] >= 3:
 #         print(i) 
 
-s = list(map(int, input("Enter a number").split()))
+# s = list(map(int, input("Enter a number").split()))
+# d = {}
+
+# for i in s:
+#     if i not in d:
+#         d[i] = 1
+#     else:
+#         d[i] += 1
+# print(d)
+
+
+# n = int(input())
+# d = {}
+
+# for i in range(n):
+#     l = input().split()
+#     d[l[0]] = l[1]
+
+# s = int(input())
+
+# for i in range(s):
+#     name = input()
+#     if name in d:
+#         print("mob:", d[name])
+#     else:
+#         print("No contact found")
+
+
+# s = int(input("Enter how much contact u to store\n"))
+# d = {}
+# for i in range(s):
+#     n = input("enter your name and contact\n").split()
+#     d[n[0]] = n[1]
+
+# m = int(input("How many times to serch\n"))
+
+# for i in range(m):
+#     name = input("Enter the which name u want to search")
+#     if name in d:
+#         print("mob:", d[name])
+#     else:
+#         print("contact not found")
+
+# n = int(input())
+# d = {}
+
+# for i in range(n):
+#     l = input().split()
+#     d[l[0].lower()] = l[1]
+
+# s = int(input())
+
+# for i in range(s):
+#     name = input().lower()
+#     if name in d:
+#         print("mob:", d[name])
+#     else:
+#         print("No contact found")
+
+'''n = input("Enter your letter\n")
+k = int(input("Enter pisition u want that letter"))
 d = {}
 
-for i in s:
+for i in n:
     if i not in d:
         d[i] = 1
     else:
         d[i] += 1
-print(d)
+
+count = 0
+for i in d:
+    if d[i] == 1:
+        count += 1
+        if k == count:
+            print(i)
+            break
+
+        # iranna'''
+
+str1 = "123"
+str1 = "c"
+print(str1)
+
+
+
+
+
+
+
 
 
 
