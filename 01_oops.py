3ce22e871e3e51374251d54545b6e6815567eec6
veeresh-code24 <veereshkanamadi@gmail.com>
2026-01-07 22:37:28 +0530

    initial work

---
+++ 01_oops.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self):
+        self.name = 'BMW'
+        self.cc = 100
+        self.colour = 'Red'
+
+
+def main():
+    c1 = Car()
+
+    if __name__ == '__main__':
+        main()
---
+++ 02_oops.py
@@ -0,0 +1,18 @@
+class Car:
+    def __init__(self,name,cc,colour):
+        self.name = name
+        self.cc = cc
+        self.colour = colour
+
+
+
+    def print_namne():
+        print(self.name)
+
+c1 = Car("chandresh","moger","white")
+print(c1.__dict__)
+c1.print_namne()
+
+
+c2=Car("shreya","chandresh","moher")
+print(c2.__dict__)
