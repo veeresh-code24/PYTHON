c2902b79199845d880348af46e5646f05e42cb7b
veeresh-code24 <veereshkanamadi@gmail.com>
2026-01-20 20:49:27 +0530

    today work

--- Exception_handling.py
+++ Exception_handling.py
@@ -12,24 +12,112 @@
 # print("Bank server securely must be closed")
 
 
-print("Execution started normally")
+# print("Execution started normally")
 
-lst = [10,20,30,40,50,0]
-l = {1:'c', 2:'java',3:'python',4:'c++'}
+# lst = [10,20,30,40,50,0]
+# l = {1:'c', 2:'java',3:'python',4:'c++'}
+
+# try:
+#     rank = int(input("Enter the rank of language "))
+#     print(l[rank])
+
+#     num = int(input("Enter the index of numerator"))
+#     den = int(input("Enter the index of denominator"))
+#     print(lst[num]/lst[den])
+# except:
+#     print("Hey something went")
+
+# print("Execution terminated normally")
+
+# a = 10
+# b = 20
+# print(a+b)
+
+'''print("Execution started normally")
 
 try:
-    rank = int(input("Enter the rank of language "))
-    print(l[rank])
 
-    num = int(input("Enter the index of numerator"))
-    den = int(input("Enter the index of denominator"))
+    lst = [10,20,30,40,0,50]
+    l = {1:"java", 2:"python", 3:"c++", 4:"c"}
+
+    r = int(input("Enter the rank of language "))
+    print(l[r])
+
+    num = int(input("Enter your numerator "))
+    den = int(input("Enter your denominator "))
     print(lst[num]/lst[den])
+
+
+
+except KeyError as a:
+    print(a)
+
+except IndexError as a:
+    print(a)
+
+except ZeroDivisionError as e:
+    print(e)
+
+except ValueError as e:
+    print(e)
+
 except:
-    print("Hey something went")
+    print("Hey something went wrong")
 
-print("Execution terminated normally")
+print("Execution terminated normally")'''
 
-a = 10
-b = 20
-print(a+b)
+'''def fun2():
+    # try:
+
+
+    print('fun2() execution started')
+
+    num = int(input("Enter your numerator"))
+    den = int(input("Enter your numerator"))
+    print(num/den)
+    # except ZeroDivisionError:
+    # print("fun2() exception handled here")
+    print('fun2 terminated normally')
+
+def fun1():
+
+    print("Fun1 started execution")
+    # try:
+
+    fun2()
+    # except:
+        # print("fun1() Exception handled here")
+
+    print("Fun1 terminated normally")
+
+def main():
+    print("main() xecution started")
+    try:
+
+
+        fun1()
+    except ZeroDivisionError:
+        print("main() Exception handled here")
+
+    print('main() terminated normally')
+
+main()'''
+
+print("Bank server secure connection established here")
+try:
+
+    loan = int(input("Enter your loan amount\n"))
+    t_d = int(input("Enter your time duration loan\n"))
+    inte = 10
+    res = (loan*t_d*inte)/100
+    print("Your loan amount is ",res)
+except:
+    print("Something went wrong")
+
+print("Bank connection securely closed here")
+
+
+
+
+
 
