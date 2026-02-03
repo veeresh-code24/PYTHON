be10d49d0b5717326da52b525fe7c241b6d6a696
veeresh-code24 <veereshkanamadi@gmail.com>
2026-02-03 23:12:27 +0530

    initial work

--- 10_loops.py
+++ 10_loops.py
@@ -1,54 +1,67 @@
 # lst = [10,20,30,40,50]
 
-# for i in range(len(lst)):
-#     print(lst)
+# i = 0
+# while i < 5:
+#     print(lst[i])
+#     i += 1
 
-# print(list(range(2,10)))
+# for i in lst:
 
-# print(list(range(10)))
+# for i in range (len(lst)):
+    # print(lst[i])
+
+    # print(i)
+    # print(lst)
 
 # balance = 15000
 # min_balance = 500
-# print("Before valance:",balance)
 
-# while balance > min_balance:
-#     balance -= 1000
+# while 500 < balance:
+#     balance  = balance - 1000
 
 # print(balance)
 
+# bal = 15500
+# min_bal = 500
 
-# balance = 15000
-# min_balance = 500
-# print("Before valance:", balance)
+# for i in range(5):
+#     bal = bal - 1000
 
-# while balance > min_balance:
-#     balance -= 1000
-#     print("After subtract:", balance)
+# print(bal)
 
-# print("Final:", balance)
+'''n = int(input("Enter your number\n"))
 
-
-n = int(input("Enter your number\n"))
-
-for i in range(2,n+1):
+for i in range(2, n+1):
     if n % i == 0:
         break
 if i == n:
-    print("it's a prime")
+    print(n, "It's a Prime number")
 else:
-    print("It;s not a prime")
+    print(n, "it's not Prime number")'''
 
+# even_sum, odd_sum = 0, 0
+# n = int(input("Enter your number\n"))
 
+# for i in range(1, n+1):
+#     if i % 2 == 0:
+#         even_sum += i
 
-even_sum,odd_sum = 0,0
+#         continue
+#     odd_sum += i
+
+# print("Sum of all even numbers: ", even_sum)
+# print("Sum of all even numbers: ", odd_sum)
+
 n = int(input("Enter your number\n"))
 
-for i in range(1,n+1):
-    if i % 2 == 0:
-        even_sum+= i
-        continue
+for i in range(2, n+1):
+    if n % i == 0:
+        break
+if i == n :
+    print("prime number")
+else:
+    print("not prime number")
 
-    odd_sum += i
 
-print(even_sum,"even_number")
-print(odd_sum,"Odd number")
+    
+
---
+++ problem.py
@@ -0,0 +1,25 @@
+# n = input("Enter thr string\n")
+
+# for i in range(0, len(n) - 3):
+#     print(n[i: i+4])
+
+
+# n = input("Enter the string\n")
+
+# for i in range(0, len(n)- 4):
+#     print(n[i : i+5])
+
+# n = input("Enter the string\n")
+
+# print(n[2:len(n) - 1])
+
+# n = input("Enter the string\n")
+
+# print(n[len(n)-2: 0: -1])
+
+n = input("Enter the string\n")
+
+print(n[len(n) - 2: 0: -1])
+
+
+
