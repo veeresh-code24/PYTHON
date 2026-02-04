681f9d609f50e83c617e34166ee797f8f43625c1
veeresh-code24 <veereshkanamadi@gmail.com>
2026-02-04 19:22:07 +0530

    initial work

--- problem.py
+++ problem.py
@@ -17,9 +17,55 @@
 
 # print(n[len(n)-2: 0: -1])
 
-n = input("Enter the string\n")
+# n = input("Enter the string\n")
 
-print(n[len(n) - 2: 0: -1])
+# print(n[len(n) - 2: 0: -1])
+
+# n = input("Enter the palindrome string")
+
+# if n == n[::-1]:
+#     print("It's a palindrome string")
+# else:
+#     print("It's not a palindrome string")
+
+# n = input("Enter the string ")
+# s_upper = ""
+
+# for i in n:
+#     if ord(i)>=97 and ord(i)<=122:
+#         s_upper += chr(ord(i)-32)
+#     else:
+#         s_upper += chr(ord(i))
+
+# print(n)
+# print(s_upper)
+
+# n = input("Enter the string\n")
+
+# s_upper = n.lower()
+# print(s_upper)
+
+
+# n = input("Enter the string\n")
+# u_case = ""
+
+# for i in n:
+#     if ord(i)>=97 and ord(i)<=122:
+#         u_case += chr(ord(i)-32)
+#     else:
+#         u_case += chr(ord(i))
+
+# print(u_case)
+# print(n)
+
+# n = input("Enter the string\n")
+
+# print(n[0:len(n)//2:]  + n[len(n)-1: (len(n)//2)-1:-1])
+
+
+
+        
+
 
 
 
