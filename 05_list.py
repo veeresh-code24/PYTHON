33f11d4b49653c32e762b99018985cff12a770d5
veeresh-code24 <veereshkanamadi@gmail.com>
2026-02-11 22:25:48 +0530

    today work

--- 05_list.py
+++ 05_list.py
@@ -49,7 +49,7 @@
 # print(i)
 # print(lst)
 
-lst = [10,20,30,40,50,30,20,30]
+# lst = [10,20,30,40,50,30,20,30]
 
 # lst = [i for i in lst if i != 30]
 # print(lst)
@@ -57,10 +57,130 @@ lst = [10,20,30,40,50,30,20,30]
 # lst[::2] = [99,99,99,99]
 # print(lst)
 
-for i in lst[:]:
-    # if i == 30 or i == 20:
-        # lst.remove(i)
+# for i in lst[:]:
+#     if i == 30 or i == 20:
+#         lst.remove(i)
 
-        lst.remove(30)
+# print(lst)
 
-print(lst)
+# while 30 in lst:
+#     lst.remove(30)
+# print(lst)
+
+# lst = [10,20,30,40,50,60]
+# print(lst)
+
+# print(lst.pop(3))
+# print(lst)
+
+# del lst[::2]
+# print(lst)
+
+# lst.insert(2, 24)
+# print(lst)
+
+# lst = [10,20,30,40,50,60,10]
+
+# print(lst)
+# print(len(lst))
+# print(max(lst))
+# print(min(lst))
+# print(sum(lst))
+
+# print(lst.count(10))
+# print(lst.index(20))
+# print(lst.index(10,2,7))
+
+
+# print(sorted(lst, reverse=False))
+
+# lst.sort(reverse=True)
+# print(lst)
+
+# lst = list(reversed(lst))
+# print(lst)
+
+# lst.reverse()
+# print(lst)
+
+# lst = [10,20,30,40,50]
+
+# for i,j in enumerate(lst,start=0):
+#     print(i,j)
+
+# for i in lst:
+#     print(i)
+
+# for i in range(0,5):
+#     print(i,lst[i])
+
+# lst = input("Enter the sum of sub list[]\n")
+# lst = eval(lst)
+
+
+# start = int(input("Enter the atarting index\n"))
+# end = int(input("Enter the ending index\n"))
+
+# res = sum(lst[start:end+1])
+# print(res)
+
+# lst = input("Enter the input[]/")
+# lst = eval(lst)
+# total = 0
+# for i in lst:
+#     total += i
+# print(total)
+
+# lst = [[10,20,30],[1,2,3],[12,1,21]]
+# total = 0
+
+# for i in lst:
+    # print(sum(i))
+
+# for i in lst:
+    # for num in i:
+        # total += num
+
+# print(total)
+
+# lst = input("Enter the strng\n")
+# lst = eval(lst)
+
+# start = int(input("Enter the strig\t"))
+# stop = int(input("Enter the stop index\t"))
+
+# res = sum(lst[start:stop+1])
+# print(res)
+
+# lst1 = [10,20,30,40,50]
+# lst2 = [600,700,30,440,50]
+
+# for i in lst1:
+#     if i not in lst2:
+#         lst2.append(i)
+# print(lst2)
+# print(lst1)
+
+# lst1 = [[10,20,30],[10,20,10],10,20]
+# total = 0
+# for i in lst1:
+
+#     for j in i:
+#         total += j
+# print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
