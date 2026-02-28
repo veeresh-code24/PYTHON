8343e5e3de937d34af2f059353dbe0c83ac93237
veeresh-code24 <veereshkanamadi@gmail.com>
2026-02-28 23:23:08 +0530

    initial work

--- while_loop.py
+++ while_loop.py
@@ -43,10 +43,76 @@ for i in s:
     else:
         uppe_str += i
 
-print(uppe_str)'''
+print(uppe_str)
 
 s = input("Enter the string\n")
 print(s.lower())
+
+url = ["hhtps.com", 'https.www.youtube.com', "https.www.com/",'https.googl.com']
+
+for i in url:
+    if i[0:5:] == "https":
+        print(i)
+
+for i in url:
+        i.append
+
+
+
+    if i[len(i)-4::] == "com/" or i[len(i)-3::] == "com":
+        print(i)
+
+s = input("Enter the string\n")
+
+low_case,upp_case,digits,spe_chr = 0,0,0,0
+
+for i in s:
+    if ord(i) >= 97 and ord(i) <= 122:
+        low_case += 1
+
+    elif ord(i) >= 65 and ord(i) <= 90:
+        upp_case += 1
+
+    elif ord(i) >= 48 and ord(i) <= 57:
+        digits += 1
+
+    else:
+        spe_chr += 1
+
+print(low_case,"is lower case")
+print(upp_case,"is upper case")
+print(digits,"is digits case")
+print(spe_chr,"is special character")
+
+from functools import reduce
+
+nums = input("Enter the numbers\n").split()
+print(nums)
+l = list(map(int, nums))
+print(l)
+
+add_nums = reduce(lambda x,y : x+y, l)
+print(add_nums)
+
+res = add_nums/len(nums)
+print(res)
+
+nums = list(map(int, input("Enter thr numbers\n").split()))
+
+res = 0
+
+for i in nums:
+    res += i
+    print(res)
+print(res/len(nums))'''
+
+
+
+
+
+
+
+
         
 
 
