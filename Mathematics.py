e63fef04ef849e7a38fcf27db5c874c3f68e26a1
veeresh-code24 <veereshkanamadi@gmail.com>
2026-03-08 20:40:38 +0530

    small work

--- Mathematics.py
+++ Mathematics.py
@@ -77,24 +77,173 @@ if __name__ == '__main__':
     main()'''
 
 
-def gcd_euclid(a,b):
+# def gcd_euclid(a,b):
 
-    while (a!=b):
-        if a>b:
-            a = a-b
-        else:
-            b = b-a
+#     while (a!=b):
+#         if a>b:
+#             a = a-b
+#         else:
+#             b = b-a
 
-    return a
+#     return a
         
-def main():
+# def main():
 
+#     a = int(input())
+#     b = int(input())
+#     print(gcd_euclid(a,b))
+
+# if __name__ == '__main__':
+    # main()
+
+'''def euclid_gcd(a,b):
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+
+    if a != 0:
+        return a
+    else:
+        return b
+    
+def main():
     a = int(input())
     b = int(input())
-    print(gcd_euclid(a,b))
+    print(euclid_gcd(a,b))
+
+if __name__ == '__main__':
+    main()'''
+
+# LCM
+
+'''def lcm(a,b):
+    res = max(a,b)
+
+    while (True):
+        if res % a == 0 and res % b == 0:
+            break
+        res += 1
+
+    return res
+
+def main():
+    a = int(input())
+    b = int(input())
+    print(lcm(a,b))
+
+if __name__ == '__main__':
+    main()'''
+
+# LCM and GCD more efficient code
+
+'''def lcm(a,b):
+    return (a*b)/euclid_gcd(a, b)
+
+def euclid_gcd(a,b):
+
+    while a!= 0 and b!= 0:
+        if a>b:
+            a = a%b
+        else:
+            b = b%a
+
+    if a!=0:
+        return a
+    else:
+        return b
+    
+def main():
+    a = int(input())
+    b = int(input())
+    print(euclid_gcd(a,b))
 
 if __name__ == '__main__':
     main()
+
+def factor(n):
+    i = 2
+
+    while n > 1:
+        while n % i == 0:
+            print(i)
+            n = n//i
+
+        i += 1
+
+def main():
+    n = int(input())
+    factor(n)
+
+if __name__ == '__main__':
+    main()'''
+
+# # Factor
+# import math
+
+# def factor(n):
+#     for i in range(1,int(math.sqrt(n)) +1):
+#         if n % i == 0:
+#             print(i)
+
+#             if i != i//n:
+#                 print(n//i)
+
+# def main():
+#     n = int(input())
+#     factor(n)
+
+# if __name__ == '__main__':
+#     main()
+
+
+# import math
+
+# def factor(n):
+
+#     for i in range(1, int(math.sqrt(n)) +1):
+#         if n % i == 0:
+#             print(i)
+#             # if i != n//i:
+#                 # print(n//i)
+
+#     for i in range(int(math.sqrt(n)),0,-1):
+#         if n % i == 0 and i != n//i :
+#             # if n // i:
+#             print(n//i)
+
+
+# def main():
+#     n = int(input())
+#     factor(n)
+
+# if __name__ == '__main__':
+#     main()
+
+# import math
+# def factor(n):
+
+#     for i in range(1,int(math.sqrt(n))+1 ):
+#         if n % i == 0:
+#             print(i)
+
+#     for i in range(int(math.sqrt(n)), 0, -1):
+
+#         if i != n//i and n % i == 0:
+#             print(n//i)
+
+# n = int(input())
+# factor(n)
+
+
+
+
+
+
+
+
+
 
 
 
