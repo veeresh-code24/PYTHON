0e09d0e14ac6b790a237a598554d530253fc0fce
veeresh-code24 <veereshkanamadi@gmail.com>
2026-03-14 12:51:26 +0530

    initail work

--- data_structure.py
+++ data_structure.py
@@ -205,7 +205,7 @@ print(a)'''
 
 # find the first and last index position if number is same 
 
-def searchrange(nums,target):
+'''def searchrange(nums,target):
     l,h,mid = 0,len(nums)-1,0
     res = [-1,-1]
 
@@ -216,11 +216,11 @@ def searchrange(nums,target):
             res[0] = mid
             h = mid-1
 
-        elif nums[mid] < target:
-            l = mid +1
+        elif target < nums[mid]:
+            h = mid - 1
 
         else:
-            h = mid-1
+            l = mid+1
     
     l,h,mid = 0,len(nums)-1,0
 
@@ -232,31 +232,72 @@ def searchrange(nums,target):
             l = mid+1
 
         elif nums[mid] < target:
-            l = mid +1
+             h = mid - 1
+
 
         else:
-            h = mid-1
+            l = mid+1
 
     return res
 
-nums = [3,4,4,4,4,4,4,4,4,6]
+nums = [3,4,4,4,4,4,4,4,6]
 target = 4
 
-print(searchrange(nums,target))
+print(searchrange(nums,target))'''
 
-    
+# Search rotated sorted arry
+
+'''def sorted_array(a,target):
+    l,h,mid = 0, len(a)-1, 0
+
+    while l <= h:
+        mid = (l+h)//2
+        if target == a[mid]:
+            return mid
+        # left side sorted
+        if a[l] <= a[mid]:
+            if a[l] <= target < a[mid]:
+                h = mid -1
+            else:
+                l = mid +1
+
+        else:
+            if a[mid] < target <= a[h]:
+                l = mid+1
+            else:
+                h = mid -1
+
+    return -1
 
 
+a = [7,8,1,2,4,5,6]
+target = 1
+print(sorted_array(a,target))'''
 
 
+def sorted_arrar(a,target):
+    l,h,mid = 0, len(a)-1,0
 
+    while l <= h:
+        mid = (l+h)//2
+        if target == a[mid]:
+            return mid
+        
+        if a[l] <= a[mid]:
+            if a[l] <= target < a[mid]:
+                h = mid -1
+            else:
+                l = mid +1
 
+        else:
+            if a[mid] < target <= a[h]:
+                l = mid+1
+            else:
+                h = mid-1
 
+    return -1
+ 
 
-
-
-
-
-
-
-
+a = [7,8,1,2,4,5,6]
+target = 4
+print(sorted_arrar(a,target))
