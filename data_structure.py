0f93b3c3af0fded2e8c25c626160126449ce8558
veeresh-code24 <veereshkanamadi@gmail.com>
2026-03-19 23:39:57 +0530

    small work

--- data_structure.py
+++ data_structure.py
@@ -539,7 +539,7 @@ print(first_last(a,key))'''
 
 # square root
 
-def square_root(n):
+'''def square_root(n):
 
     if n == 0 or n == 1:
         return n
@@ -564,7 +564,54 @@ def square_root(n):
 
     return res
 n = 25
-print(square_root(n))
+print(square_root(n))'''
+
+def find_median(ar1,ar2):
+    i,j,k = 0,0,0
+    m = []
+
+    while i < len(ar1) and j< len(ar2):
+        if ar1[i] < ar2[j]:
+            m.append(ar1[i])
+            i += 1
+            k += 1
+        else:
+            m.append(ar2[j])
+            j += 1
+            k += 1
+
+    while i < len(ar1):
+        m.append(ar1[i])
+        i += 1
+        k += 1
+
+    while j < len(ar2):
+        m.append(ar2[j])
+        j += 1
+        k += 1
+    
+    mid = len(m)//2
+
+    if len(m)%2 == 0:
+        return (m[mid] + m[mid-1])/2
+    else:
+        return m[mid]
+
+
+
+def main():
+
+    ar1 = [1,3,8,17]
+    ar2 = [5,6,7,19,21,25]
+    print(find_median(ar1,ar2))
+
+
+if __name__ == '__main__':
+
+    main()
+
+
+
 
 
 
