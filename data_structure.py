a8391a905ed1ad854a92f1e21f8d1b51f58d23ae
veeresh-code24 <veereshkanamadi@gmail.com>
2026-03-20 18:46:06 +0530

    initial work

--- data_structure.py
+++ data_structure.py
@@ -566,50 +566,70 @@ print(first_last(a,key))'''
 n = 25
 print(square_root(n))'''
 
-def find_median(ar1,ar2):
-    i,j,k = 0,0,0
-    m = []
+# def find_median(ar1,ar2):
+#     i,j,k = 0,0,0
+#     m = []
 
-    while i < len(ar1) and j< len(ar2):
-        if ar1[i] < ar2[j]:
-            m.append(ar1[i])
-            i += 1
-            k += 1
-        else:
-            m.append(ar2[j])
-            j += 1
-            k += 1
+#     while i < len(ar1) and j< len(ar2):
+#         if ar1[i] < ar2[j]:
+#             m.append(ar1[i])
+#             i += 1
+#             k += 1
+#         else:
+#             m.append(ar2[j])
+#             j += 1
+#             k += 1
 
-    while i < len(ar1):
-        m.append(ar1[i])
-        i += 1
-        k += 1
+#     while i < len(ar1):
+#         m.append(ar1[i])
+#         i += 1
+#         k += 1
 
-    while j < len(ar2):
-        m.append(ar2[j])
-        j += 1
-        k += 1
+#     while j < len(ar2):
+#         m.append(ar2[j])
+#         j += 1
+#         k += 1
     
-    mid = len(m)//2
+#     mid = len(m)//2
 
-    if len(m)%2 == 0:
-        return (m[mid] + m[mid-1])/2
-    else:
-        return m[mid]
-
-
-
-def main():
-
-    ar1 = [1,3,8,17]
-    ar2 = [5,6,7,19,21,25]
-    print(find_median(ar1,ar2))
+#     if len(m)%2 == 0:
+#         return (m[mid] + m[mid-1])/2
+#     else:
+#         return m[mid]
 
 
-if __name__ == '__main__':
 
-    main()
+# def main():
 
+#     ar1 = [1,3,8,17]
+#     ar2 = [5,6,7,19,21,25]
+#     print(find_median(ar1,ar2))
+
+
+# if __name__ == '__main__':
+
+#     main()
+
+
+def selection_sort(arr,n):
+    for i in range(n):
+        min_index = i
+        for j in range(i +1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index], arr[i]
+        # print(arr[i], end= " ")
+
+arr = [12,21,34,32,21]
+n = 5
+selection_sort(arr,n)
+
+for i in range(n):
+    print(arr,end = " ")
+
+
+
+        
 
 
 
--- sort.py
+++ sort.py
@@ -35,24 +35,113 @@
 
 
 
-def insertion_sort(nums):
-    for i in range(1,len(nums)):
-        key=nums[i]
-        j=i-1
-        while j > 0 and key < nums[j]:
-            nums[j+1]= nums[j]
+# def insertion_sort(nums):
+#     for i in range(1,len(nums)):
+#         key=nums[i]
+#         j=i-1
+#         while j > 0 and key < nums[j]:
+#             nums[j+1]= nums[j]
 
         
 
-        nums[j+1]=key
+#         nums[j+1]=key
     
 
-    return nums
+#     return nums
 
 
 
-nums=[1,0,1,4,5,3,0]
-print(insertion_sort(nums))
+# nums=[1,0,1,4,5,3,0]
+# print(insertion_sort(nums))
+
+
+# bubble sort
+
+'''def bubble_sort(arr,n):
+    for i in range(n):
+        did_swap = 0
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                # arr[j],arr[j+1] = arr[j+1],arr[j]
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+
+                did_swap = 1
+
+        if did_swap == 0:
+            break
+        print("didn't run")
+
+        
+    print(*arr)
+
+arr = [2,3,4,5,6]
+n = 5
+bubble_sort(arr,n)'''
+
+# insertion sort
+'''def insertion_sort(arr,n):
+
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+
+        while j >=0 and arr[j] > key:
+             arr[j+1] = arr[j]
+             j -= 1
+
+        arr[j+1] = key
+    return arr
+
+arr = [3,5,8]
+n = 3
+print(insertion_sort(arr,n))'''
+
+# def bubble_sort(arr,n):
+#     for i in range(n-1,-1,-1):
+#         for j in range(i):
+#             if arr[j] > arr[j+1]:
+#                 arr[j],arr[j+1] = arr[j+1],arr[j]
+
+#     print(*arr)
+
+# arr = [1,2,64,54,23]
+# n = 5
+# bubble_sort(arr,n)
+
+
+def insertion_sort(arr,n):
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+
+    print(*arr)
+
+arr = [2,1,34,53,12,1]
+n = 6
+insertion_sort(arr,n)
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
