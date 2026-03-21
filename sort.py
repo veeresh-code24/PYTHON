8246d10024199a5b3a55bcb665a784cafc3b9ccf
veeresh-code24 <veereshkanamadi@gmail.com>
2026-03-22 00:18:40 +0530

    revision work

--- sort.py
+++ sort.py
@@ -111,22 +111,44 @@ print(insertion_sort(arr,n))'''
 # bubble_sort(arr,n)
 
 
+# def insertion_sort(arr,n):
+#     for i in range(1,n):
+#         key = arr[i]
+#         j = i-1
+
+#         while j >= 0 and arr[j] > key:
+#             arr[j+1] = arr[j]
+#             j -= 1
+
+#         arr[j+1] = key
+
+#     print(*arr)
+
+# arr = [2,1,34,53,12,1]
+# n = 6
+# insertion_sort(arr,n)
+
+# insertion sort
+
 def insertion_sort(arr,n):
-    for i in range(1,n):
+
+    for i in range(n):
         key = arr[i]
         j = i-1
 
-        while j >= 0 and arr[j] > key:
+        while j>= 0 and arr[j] > key:
             arr[j+1] = arr[j]
             j -= 1
 
         arr[j+1] = key
 
-    print(*arr)
-
-arr = [2,1,34,53,12,1]
-n = 6
+        print(*arr)
+arr = [1,8,4,3,2]
+n = 5
 insertion_sort(arr,n)
+
+
+
 
  
 
--- test.py
+++ test.py
@@ -1,66 +1,27 @@
-# def largest(arr):
-#     return arr[-1]
-
-    
-# arr = [3,2,1,15,2]
-# arr.sort()
-# print(largest(arr))
-
-
-# def largest_element(arr):
+# def selection_sort(arr):
 #     n = len(arr)
-#     largest = arr[0]
 
-#     for i in range(1,n):
-#         if arr[i] > largest:
-#             largest = arr[i]
+#     for i in range(n):
+#         min_value = i
+#         for j in range(i+1,n):
+#             if arr[j] < arr[min_value]:
+#                 min_value = j
 
-#     return largest
-
-# arr = [3,2,1,15,2]
-# print(largest_element(arr))
-
-
-# def second_largest(arr):
-#     largest = arr[-1]
-
-#     for i in range(n-1,-1,-1):
-#         if arr[i] != largest:
-#             return arr[i]
-
-# arr = [1,2,15,7,7]
-# n = 5
-# arr.sort()
-# print(second_largest(arr))
+#         arr[i],arr[min_value] = arr[min_value],arr[i]
+#         print(*arr)
 
 
-def fir_largest(arr,n):
-    fir_largest = arr[0]
-    second_largest = float('inf')
+# arr = [4,3,1,7,2]
+# selection_sort(arr)
+# print(arr)
 
-    for i in range(1,n):
-        if arr[i] < fir_largest:
-            second_largest = fir_largest
-            fir_largest = arr[i]
+def bubble_sort(arr,n):
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j+1],arr[j] = arr[j],arr[j+1]
 
-        elif arr[i] > second_largest and arr[i] < fir_largest:
-            second_largest = arr[i]
-
-    return second_largest
-
-
-    # for i in range(1,n):
-    #     if arr[i] > fir_largest:
-    #         fir_largest = arr[i]
-
-    # second_largest = -1
-
-    # for i in range(n):
-    #     if arr[i] > second_largest and arr[i] != fir_largest:
-    #         second_largest = arr[i]
-
-    # return second_largest,fir_largest
-
-arr = [3,2,110,500,7,6]
-n = 6
-print(fir_largest(arr,n))
+    print(*arr)
+arr = [1,8,3,2,5]
+n = 5
+bubble_sort(arr,n)
