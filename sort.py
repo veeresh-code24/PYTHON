5fa68c1ec44a501b10dae130a6982615595bda71
veeresh-code24 <veereshkanamadi@gmail.com>
2026-03-25 22:32:15 +0530

    initial work

--- sort.py
+++ sort.py
@@ -1,16 +1,49 @@
-def max_consec(nums):
-    maxi = 0
-    count = 0
+# def max_consec(nums):
+#     maxi = 0
+#     count = 0
+
+#     for i in range(len(nums)):
+#         if nums[i] == 1:
+#             count += 1
+#             if count > maxi:
+#                 maxi = count
+
+#         else:
+#             count = 0
+#     return maxi
+
+# nums = [1,1,0,1,1,1]
+# print(max_consec(nums))
+
+# def single(nums):
+
+#     for i in range(len(nums)):
+#         num = nums[i]
+#         count = 0
+
+#         for j in range(len(nums)):
+#             if nums[j] == num:
+#                 count += 1
+
+#         if count == 1:
+#             return num
+
+
+
+# nums = [1, 2, 2, 4, 3, 1, 4]
+# print(single(nums))
+
+def sub_array(nums):
 
     for i in range(len(nums)):
-        if nums[i] == 1:
-            count += 1
-            if count > maxi:
-                maxi = count
+        su = 0
+        for j in range(i, len(nums)):
+            su = nums[j] + su
+            su = su == key
+            print(su)
+   
+            print(nums[i:j+1])
+key = 15
+nums = [1, 2, 2, 4, 3, 1, 4]
+sub_array(nums)
 
-        else:
-            count = 0
-    return maxi
-
-nums = [1,1,0,1,1,1]
-print(max_consec(nums))
