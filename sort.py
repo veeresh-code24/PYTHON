cdc599e4fdfda7642fe21a72c39fad36237c4b2f
veeresh-code24 <veereshkanamadi@gmail.com>
2026-03-27 19:20:51 +0530

    initial work

--- sort.py
+++ sort.py
@@ -24,20 +24,75 @@
 # print(sub_array(nums,k))
 
 
-def rotate_arr(nums,k):
+# def rotate_arr(nums,k):
+#     n = len(nums)
+#     temp = nums[0:k]
+
+#     for i in range(k,n):
+#         nums[i-k] = nums[i]
+#     x = 0
+#     for h in range(n-k,n):
+#         nums[h] = temp[h-(n-k)]
+#         x += 1
+
+#     return nums
+
+# def rotate_array(nums,k):
+#     n = len(nums)
+#     k = k%n
+
+#     nums[:k] = reversed(nums[:k])
+#     nums[k:] = reversed(nums[k:])
+#     nums[:] = reversed(nums)
+
+
+
+#     return nums
+
+
+
+# nums = [1,2,3,4,5,6]
+# k = 3
+# print(rotate_array(nums,k))
+
+# def two_sum(nums,target):
+#     n = len(nums)
+
+#     for i in range(n):
+#         for j in range(i+1,n):
+#             if nums[i] + nums[j] == target:
+#                 return i,j
+            
+
+# nums = [2,6,5,8,11]
+# target = 14
+# print(two_sum(nums,target))
+
+
+def two_sum(nums,target):
     n = len(nums)
-    temp = nums[0:k]
+    left = 0
 
-    for i in range(k,n):
-        nums[i-k] = nums[i]
-    x = 0
-    for h in range(n-k,n):
-        nums[h] = temp[h-(n-k)]
-        x += 1
+    right = n-1
 
-    return nums
+    while left < right:
+        sum_arr = nums[left] + nums[right]
+        if sum_arr == target:
+            return "Yes"
+        
+        elif sum_arr < target:
+            left += 1
+        else:
+            right -=1
 
-nums = [1,2,3,1,1,1,1,4,2,3]
-k = 5
-print(rotate_arr(nums,k))
+    return "No"
+        
+
+
+nums = [2,5,6,8,11]
+target = 14
+print(two_sum(nums,target))
+        
+    
+
 
