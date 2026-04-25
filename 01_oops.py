f24a17869af88a46a449b5a9a52429b25574f46e
veeresh-code24 <veereshkanamadi@gmail.com>
2026-04-25 18:53:41 +0530

    python work

--- 01_oops.py
+++ 01_oops.py
@@ -1,221 +1,91 @@
-# class Car:
-#     def __init__(self):
-#         self.name = 'BMW'
-#         self.cc = 2100
-#         self.colour = 'Blue'
+# Creting a Object
 
-#     def start_engine(self):
-#         print("Startting a engine....")
+'''class Car:
 
-#     def shift_gear(self):
-#         print("hifting a gear....")
+    def __init__(self):
+        self.brand = 'BMW'
+        self.cc = 1000
+        self.colour = 'blue'
 
-#     def accelerate(self):
-#         print("Accelearating....")
+    def start_engine(self):
+            print(self.brand, "starting the engine")
 
+    def accelerate(self):
+            print(self.brand,"accelerating the engine")
 
-# def main():
-#     c1 = Car()
-#     print(c1.name)
-#     print(c1.cc)
-#     print(c1.colour)
-#     c1.start_engine()
-#     c1.shift_gear()
-#     c1.accelerate()
+    def show_colour(self):
+            print(self.colour,"is the car colour")
 
-
-# if __name__ == '__main__':
-#     main()
-
-# class Bus:
-#     def __init__(self):
-#         self.brand = 'KSRTC'
-#         self.cc = 1000
-#         self.colour = 'Red'
-    
-#     def self_brand(self):
-#         print(self.brand, "It's a government bus")
-
-#     def cc_engine(self):
-#         print(self.cc,"It's a 1000 capacity is there")
-
-#     def bus_colour(self):
-#         print(self.colour,"IT'a bus a colouer is Blue")
-
-# def main():
-#     c1 = Bus()
-#     print(c1.brand)
-#     print(c1.cc)
-#     print(c1.colour)
-#     c1.self_brand()
-#     c1.cc_engine()
-#     c1.bus_colour()
-
-#     c2 = Bus()
-#     print(c2.brand)
-#     print(c2.cc)
-#     print(c2.colour)
-#     c2.self_brand()
-#     c2.cc_engine()
-#     c2.bus_colour()
-
-# if __name__ == '__main__':
-#     main()
-
-
-# class Bus:
-#     def __init__(self):
-#         self.brand = 'KSRTC'
-#         self.cc = 1000
-#         self.colour = 'Red'
-
-#     def name(self):
-#         print(self.brand,"is the karnataka bus")
-
-#     def engine_capacity(self):
-#         print(self.cc,"is the engine capacity")
-
-#     def bus_colour(self):
-#         print(self.colour,"is the bus colour")
-
-
-# def main():
-#     c1 = Bus()
-#     print(c1.brand)
-#     print(c1.cc)
-#     print(c1.colour)
-#     c1.name()
-#     c1.engine_capacity()
-#     c1.bus_colour()
-
-#     c2 = Bus()
-#     print(c2.brand)
-#     print(c2.cc)
-#     print(c2.colour)
-#     c2.name()
-#     c2.engine_capacity()
-#     c2.bus_colour()
-
-# if __name__ == '__main__':
-#     main()
-
-# class Cricket:
-#     def __init__(self,name,team,run):
-#         self.name = name
-#         self.team = team
-#         self.run = run
-
-#     def batting(self):
-#         print(self.name,'batting is good')
-
-#     def hitman(self):
-#         print(self.name,'is the hitting a more six')
-
-#     def hig_score(self):
-#         print(self.name,'is the highest score in india')
-
-# def main():
-#     vir = Cricket('Virat Kohli','India',13000)
-#     print(vir.name)
-#     print(vir.team)
-#     print(vir.run)
-
-#     vir.batting()
-#     vir.hitman()
-#     vir.hig_score()
-
-#     dhoni = Cricket('Dhoni','CSK',12200)
-#     print(dhoni.name)
-#     print(dhoni.team)
-#     print(dhoni.run)
-
-#     dhoni.batting()
-#     dhoni.hitman()
-#     dhoni.hig_score()
-
-# if __name__ == '__main__':
-#     main()
-
-'''class Cricket:
-    def __init__(self,name,team,score):
-        self.name = name
-        self.team = team
-        self.score = score
-
-    def batting(self):
-        print(self.name,'is the best player')
-
-    def hiter(self):
-        print(self.team,'team is won the cup')
-
-    def fielding(self):
-        print(self.name,'is the best player')
-
-    def display(self):
-        print(self.name)
-        print(self.team)
-        print(self.score)
-        print(self.age)
-        print(self.jersay_no)
-        print(self.food)
-        print(self.ride)
 
 def main():
-    vir = Cricket('Dhoni','CSK',12000)
+    c1 = Car()
+    print(c1.brand)
+    print(c1.cc)
+    print(c1.colour)
+    c1.start_engine()
+    c1.accelerate()
+    c1.car_colour()
 
-    vir.age = 35
-    vir.jersay_no = 18
-
-    setattr(vir,'food','veg')
-    setattr(vir,'ride','bike')
-
-    print(getattr(vir,'name'))
-    print(getattr(vir,'age'))
-
-    print(hasattr(vir,'name'))
-    print(hasattr(vir,'male'))
-
-    print(vir.__dict__)
-    print(vir.jersay_no)
-    print(vir.__dict__['name'])
-
-
-
-
-    vir.display()
-    # vir.age = 54
-    # vir.jersay_no = 7
-    # setattr(vir,'age',37)
-    # setattr(vir,'jersay_no',47)
-
-    # print(getattr(vir,'name'))
-    # print(vir.name)
-
-    # print(hasattr(vir,'name'))
-    # print(hasattr(vir,'gender'))
-
-    # print(vir.__dict__)
-    # print(vir.name)
-    # print(vir.__dict__['name'])
-    # vir.display()
-
-    # vir.batting()
-    # vir.hiter()
-    # vir.fielding()
-
-    # dhon = Cricket('Virat','RCB',7000)
-    # dhon.age = 45
-    # dhon.jersay_no = 18
-    # dhon.display()
-
-
-    # dhon.batting()
-    # dhon.hiter()
-    # dhon.fielding()
 
 
 if __name__ == '__main__':
     main()'''
+
+# Differnt object with state, behaviour,property and 
+
+class FootBaller:
+    def __init__(self,name,team,goal):
+        self.name = name
+        self.team = team
+        self.goal = goal
+
+    def shooting(self):
+        print(self.name, 'is a shooting')
+
+    def passing(self):
+        print(self.name, 'is passing the ball')
+
+    def running(self):
+        print(self.name, 'is a running')
+
+    def display(self):
+        print(self.name)
+        print(self.team)
+        print(self.goal)
+        print(self.age)
+        print(self.jersey_no)
+
+def main():
+    cr = FootBaller('critiano','juventu','749')
+    # cr.display()
+
+    setattr(cr,'age',37)
+    setattr(cr,'jersey_no',7)
+    # cr.age = 37
+    # cr.jersey_no = 7
+    cr.display()
+    # print(getattr(cr,'name'))
+    # print(cr.name)
+
+    print(hasattr(cr,'name'))
+    print(hasattr(cr,'iranna'))
+
+    print(cr.__dict__)
+    print(cr.__dict__['name'])
+    print(cr.name)
+
+    cr.shooting()
+    cr.passing()
+    cr.running()
+
+    # messi = FootBaller('messi','portgu','856')
+    # messi.display()
+    # messi.running()
+    # messi.shooting()
+    # messi.passing()
+
+if __name__ == '__main__':
+    main()
 
 
 
